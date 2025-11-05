65c39952a62cf79d51f73d09c80deabe9cc0a934
bramcaeyman <bram.caeyman@ugent.be>
2025-11-05 22:32:15 +0100

    Update config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class config:
+    SECRET_KEY = 'your_secret_key_here'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:port/dbname'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
