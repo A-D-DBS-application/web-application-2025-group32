cefc8da3ae687135e02e69638d1bbff7d202f762
bramcaeyman <bram.caeyman@ugent.be>
2025-11-08 14:42:10 +0100

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class config:
+    SECRET_KEY = 'f2c4b8a8d73a4b73b96d78e5fbd9c9a1'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:groep32mvp1@db.knxcqgoealvgfqcuffep.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
