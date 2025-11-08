1ca49394a3a924fdaf5cf7969179d215ca460d1a
bramcaeyman <bram.caeyman@ugent.be>
2025-11-08 14:42:30 +0100

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-class config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:port/dbname'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
